19feeb01576d8104ff2e4566d147f1baa7e74e40
Will Welker <wwelker@gmail.com>
2019-01-12 20:36:42 -0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,7 +3,5 @@
 # auto sync files to Github repo
 
 git add *
-# commit message is first argument when executing script
-# example: ./sync.sh added-wheel 
-git commit -m '$1'
+git commit -m 'autosync'
 git push
