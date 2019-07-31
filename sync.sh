8ba0c846b19e05b0bce5a1f13c42b332f29a55ff
Will <wwelker@gmail.com>
2019-07-31 05:32:18 -0700

    autosync

--- sync.sh
+++ sync.sh
@@ -5,5 +5,5 @@
 # To add, commit and push all changes, issue:~$ ./sync.sh commit message up to 9 words
 
 git add *
-git commit -m "autosync $1 $2 $3 $4 $5 $6 $7 $9"
+git commit -m "autosync $1 $2 $3 $4 $5 $6 $7 $8 $9"
 git push
